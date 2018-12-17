59ba4e4eb5bb6576f72a2d016913bcfc4c180e46
peccu <peccul@gmail.com>
2018-12-18 00:23:20 +0900

    add vue and bootstrap-vue and its sample

---
+++ src/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="app">
+  <h1>{{ msg }}</h1>
+  <input
+    id="repository"
+    type="text"
+    style="width: 50em"
+    title="Tip: enter a private repo URL to see the credentialManager plugin prompt for a password."
+    />
+  <button type="button" id="cloneButton">Clone</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983;
+  }
+</style>
